2ee972a2f8b8f4acb95933c7f6480ab2c76a6530
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-06-14 14:36:00 -0300

    Static updates

--- my_django_resume/main/urls.py
+++ my_django_resume/main/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
-
 from . import views
 
-
-app_name = 'my_django_resume.main'
-
+app_name = "main"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='home'),
-    path('contact/', views.ContactView.as_view(), name='contact'),
-    path('portfolio/', views.PortfolioView.as_view(), name='portfolios'),
-    path('portfolio/<slug:slug>', views.PortfolioView.as_view(), name='portfolio'),
-    path('blog/', views.BlogView.as_view(), name='blogs'),
-    path('blog/<slug:slug>', views.BlogView.as_view(), name='blog')
+    path('', views.IndexView.as_view(), name="home"),
+    path('contact/', views.ContactView.as_view(), name="contact"),
+    path('portfolio/', views.PortfolioView.as_view(), name="portfolios"),
+    path('portfolio/<slug:slug>', views.PortfolioDetailView.as_view(), name="portfolio"),
+    path('blog/', views.BlogView.as_view(), name="blogs"),
+    path('blog/<slug:slug>', views.BlogDetailView.as_view(), name="blog"),
 ]
--- my_django_resume/urls.py
+++ my_django_resume/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('my_django_resume.main.urls', namespace='main'))
+    path('', include('my_django_resume.main.urls', namespace="main")),
+    path(r'^media/(?P<path>.*)$', serve,
+         {'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,
+         {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
